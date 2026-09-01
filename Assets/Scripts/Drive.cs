9374ea7bab036e994c5f36fc3f1fc234712c235f
EManofE <thepreacherofe@gmail.com>
2026-09-01 19:09:28 +0100

    Acceleration Step 1

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -7,6 +7,11 @@ public class Drive : MonoBehaviour
     public float speed = 1.0f;
     public float rotationSpeed = 100.0f;
 
+    public Transform transgun;
+    public Transform gun;
+    public GameObject bullet;
+    
+
     void Update()
     {
         // Get the horizontal and vertical axis.
@@ -20,10 +25,24 @@ public class Drive : MonoBehaviour
         rotation *= Time.deltaTime;
 
         // Move translation along the object's z-axis
-        //transform.Translate(0, 0, translation);
-        transform.Translate(0, 0, speed * Time.deltaTime);
+        transform.Translate(0, 0, translation);
+        //transform.Translate(0, 0, speed * Time.deltaTime);
 
         // Rotate around our y-axis
-        //transform.Rotate(0, rotation, 0);
+        transform.Rotate(0, rotation, 0);
+
+        if(Input.GetKey(KeyCode.T))
+        {
+            transgun.RotateAround(transgun.position, transgun.right, -2);
+        }
+        else if (Input.GetKey(KeyCode.G))
+        {
+            transgun.RotateAround(transgun.position, transgun.right, 2);
+        }
+        else if(Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(bullet, gun.position,gun.rotation);
+        }
+
     }
 }
