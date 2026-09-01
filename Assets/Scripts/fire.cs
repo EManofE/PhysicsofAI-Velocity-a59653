d73c8e2527c5852c74dd658a2189767b633d10b8
EManofE <thepreacherofe@gmail.com>
2026-09-01 19:52:42 +0100

    Calculating Trajectories Step 1

--- Assets/Scripts/fire.cs
+++ Assets/Scripts/fire.cs
@@ -6,44 +6,23 @@ public class fire : MonoBehaviour
     public GameObject bullet;
     public GameObject turret;
     public GameObject enemy;
+    float rotspeed = 2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Shoot()
     {
         Instantiate(bullet, turret.transform.position, turret.transform.rotation);
     }
 
-    Vector3 CalculateTrajectory()
-    {
-    Vector3 p = enemy.transform.position - this.transform.position;
-    Vector3 v = enemy.transform.forward * enemy.GetComponent<Drive>().speed;
-    float s = bullet.GetComponent<movebullet>().speed;
-    float a = Vector3.Dot(v, v) - s * s;
-    float b = Vector3.Dot(p, v);
-    float c = Vector3.Dot(p, p);
-    float d = b * b- a * c;
-    if (d < 0.1f) return Vector3.zero;
-    float sqrt = Mathf.Sqrt(d) ;
-    float t1 = (-b - sqrt) / c;
-    float t2 = (-b + sqrt) / c;
-    float t = 0;
-    if (t1 < 0 && t2 < 0) return Vector3.zero;
-    else if (t1 <0)t= t2;
-    else if (t2 < 0)t = t1;
-    else
-    {
-        t = Mathf.Max(new float[] { t1, t2 });
-    }
-    return t * p + v;
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0,direction.z));
+        this.transform.rotation=Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * rotspeed);
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 aimat = CalculateTrajectory();
-            if(aimat != Vector3.zero)
-                this.transform.forward=CalculateTrajectory();
             Shoot();
         }
     }
