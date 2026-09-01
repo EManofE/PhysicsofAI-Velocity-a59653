4c5e7dc4e5a089e6920356eeea0d7a84e2620563
EManofE <thepreacherofe@gmail.com>
2026-09-01 18:41:19 +0100

    Step 3

--- Assets/Scripts/fire.cs
+++ Assets/Scripts/fire.cs
@@ -5,15 +5,35 @@ public class fire : MonoBehaviour
 
     public GameObject bullet;
     public GameObject turret;
+    public GameObject enemy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Shoot()
     {
         Instantiate(bullet, turret.transform.position, turret.transform.rotation);
     }
 
-    void CalculateTrajectory()
+    Vector3 CalculateTrajectory()
     {
-        
+    Vector3 p = enemy.transform.position - this.transform.position;
+    Vector3 v = enemy.transform.forward * enemy.GetComponent<Drive>().speed;
+    float s = bullet.GetComponent<movebullet>().speed;
+    float a = Vector3.Dot(v, v) - s * s;
+    float b = Vector3.Dot(p, v);
+    float c = Vector3.Dot(p, p);
+    float d = b * b- a * c;
+    if (d < 0.1f) return Vector3.zero;
+    float sqrt = Mathf.Sqrt(d) ;
+    float t1 = (-b - sqrt) / c;
+    float t2 = (-b + sqrt) / c;
+    float t = 0;
+    if (t1 < 0 && t2 < 0) return Vector3.zero;
+    else if (t1 <0)t= t2;
+    else if (t2 < 0)t = t1;
+    else
+    {
+        t = Mathf.Max(new float[] { t1, t2 });
+    }
+    return t * p + v;
     }
 
     // Update is called once per frame
@@ -21,7 +41,9 @@ public class fire : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            CalculateTrajectory();
+            Vector3 aimat = CalculateTrajectory();
+            if(aimat != Vector3.zero)
+                this.transform.forward=CalculateTrajectory();
             Shoot();
         }
     }
